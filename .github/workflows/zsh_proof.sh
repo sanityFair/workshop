b4d7528f5a9d288d10acc3a9f11146458c9e5da9
sanityFair <106737730+sanityFair@users.noreply.github.com>
2023-11-17 12:18:45 +0300

    Update zsh_proof.sh

--- .github/workflows/zsh_proof.sh
+++ .github/workflows/zsh_proof.sh
@@ -1,1 +1,18 @@
+#!/usr/bin/env zsh
 
+if [[ $(command -v zsh) ]]; then
+  echo "ZSH exists."
+  node -v
+  npm audit
+else 
+  echo "ZSH isn't found."
+  exit 1
+fi
+
+if [[ "$ZSH_NAME" == "zsh" ]]; then
+  echo "ZSH seems to be being used as this shell."
+  exit 0
+else
+  echo "ZSH is (probably) not the shell being used."
+  exit 1
+fi
